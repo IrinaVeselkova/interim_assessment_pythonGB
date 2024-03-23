e4ed87cb4dc1fcbfc6d6eac697a9871c53c8e526
Veselkova Irina <veselkovaiv@yandex.ru>
2024-03-23 20:49:56 +0300

    create search and show methods

--- interface.py
+++ interface.py
@@ -20,22 +20,22 @@ def interface():
     while flag != True:
         input_menu = input("Выберите пункт меню: => ")
         flag = is_correct_input(input_menu,6)
-
-    if input_menu == 1:
+    if int(input_menu) == 1:
         add_note()
-    elif input_menu == 2:
+    elif int(input_menu) == 2:
         interface_search()
-    elif input_menu == 3:
+    elif int(input_menu) == 3:
         change_note()
-    elif input_menu == 4:
+    elif int(input_menu) == 4:
         show_notes()
-    elif input_menu == 5:
+    elif int(input_menu) == 5:
         remove_note()
-    elif input_menu == 6:
+    elif int(input_menu) == 6:
         clear()
 
 def interface_search():
     search_in = input("Введите данные для поиска: =>")
     print("Вот что удалось найти: ")
+    print("*"*60)
     search_note(search_in)
     
--- methods_notes.py
+++ methods_notes.py
@@ -16,23 +16,23 @@ def add_note():
     id_note = chr(random.randint(65, 90)) + \
         chr(random.randint(65, 90)) + str(random.randint(0, 1001))
     theme_note = input("Введите краткое описание заметки: => ")
-    if len(theme_note) > 20:
-        theme_note = theme_note[:21]
+    if len(theme_note) > 30:
+        theme_note = theme_note[:31]
     if theme_note == "" or theme_note == None or theme_note == ' ':
         theme_note = "Тема отсутствует"
     print("Текст заметки (после окончания ввода нажмите ESC): => ")
-    text_note = ''
+    text_note = 'Нет заметки'
     for line in sys.stdin:
         text_note = text_note+line
         key = ord(getch())
         if key == 27:
             break
-    note["id_note"] = id_note
-    note["theme"] = theme_note
+    note["id "] = id_note
+    note["тема"] = theme_note
     datetime_note = datetime.now().strftime("%d.%m.%Y %H:%M:%S")
-    note["date_time"] = datetime_note
-    note["change_data_time"] = datetime_note
-    note["text"] = text_note
+    note["дата_и_время заметки"] = datetime_note
+    note["дата_и_время_изменения_заметки"] = datetime_note
+    note["заметка"] = text_note
     with open(file_name, "a", encoding='utf-8') as notes_file:
         json.dump(note, notes_file,ensure_ascii=False)
         notes_file.write('\n')
@@ -71,14 +71,34 @@ def search_note(search_in):
             for val in v.values():
                 if search_in in val or val in search_in:
                     for key,value in v.items():
-                        print(key,":",value)
+                        if key=="заметка":
+                            print("*"*60)
+                            print(value.rstrip())
+                            
+                        else:
+                            print(f"{key}{" "*(30-len(key))} : {value.rstrip()}")
+                    print()
+                    break
 
 # показать все заметки
 
 
 def show_notes():
-    pass
+    with open(file_name,"r",encoding="utf-8") as notes_file:
+        data=[json.loads(jline) for jline in notes_file]
+        for v in data:
+            for key,value in v.items():
+                if key=="заметка":
+                    print("*"*60)        
+                    print(value.rstrip())        
+                else:            
+                    print(f"{key}{" "*(30-len(key))} : {value.rstrip()}")    
+                    print()        
+            print('\n',"*"*60)             
+                    
+                
+                    
 
 
 # add_note()
-search_note()
+# search_note("23.03")
